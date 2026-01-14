bded1da7608857dd2e7470660119a9eb0dd06d09
Janak Patidar <janakpatidar757@gmail.com>
2026-01-14 18:31:56 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from "@/context/AuthContext";
 import { initializeFirebase } from "@/firebase";
 import { FirebaseProvider } from "@/firebase/provider";
 import { FirebaseErrorListener } from "@/components/FirebaseErrorListener";
+import { Toaster } from "@/components/ui/toaster";
 
 // Initialize Firebase services once on the client.
 const { firebaseApp, auth, firestore } = initializeFirebase();
@@ -24,7 +25,3 @@ export function FirebaseClientProvider({
     </FirebaseProvider>
   );
 }
-
-// We need to re-export the Toaster here because it was moved from the layout
-// and this is the most convenient shared client boundary.
-import { Toaster } from "@/components/ui/toaster";
