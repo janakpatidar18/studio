f5744b886c125f230383176dc9501e524a8bc32e
Janak Patidar <janakpatidar757@gmail.com>
2026-01-14 18:28:56 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // This file is intentionally left blank. It will be populated with your Firebase project's configuration.
 // This is a placeholder to allow the application to compile.
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID"
+  projectId: "studio-7807629179-4ef8b",
+  appId: "1:695547801416:web:0266b7b9974f9da59b4d7d",
+  apiKey: "AIzaSyD_GzgEZfiZd-PEusqFNhvJOOaFOmkKGCc",
+  authDomain: "studio-7807629179-4ef8b.firebaseapp.com",
+  storageBucket: "studio-7807629179-4ef8b.appspot.com",
+  messagingSenderId: "695547801416",
+  measurementId: ""
 };
