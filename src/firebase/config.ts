2c590fdd26e0ec3b0ef8c1da55e6c451c3e1d31c
Janak Patidar <janakpatidar757@gmail.com>
2026-01-16 16:07:19 +0000

    Set up a Firebase project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // This file is intentionally left blank. It will be populated with your Firebase project's configuration.
 // This is a placeholder to allow the application to compile.
 export const firebaseConfig = {
-  projectId: "studio-7807629179-4ef8b",
-  appId: "1:695547801416:web:0266b7b9974f9da59b4d7d",
-  apiKey: "AIzaSyD_GzgEZfiZd-PEusqFNhvJOOaFOmkKGCc",
-  authDomain: "studio-7807629179-4ef8b.firebaseapp.com",
-  storageBucket: "studio-7807629179-4ef8b.appspot.com",
-  messagingSenderId: "695547801416",
+  projectId: "",
+  appId: "",
+  apiKey: "",
+  authDomain: "",
+  storageBucket: "",
+  messagingSenderId: "",
   measurementId: ""
 };
